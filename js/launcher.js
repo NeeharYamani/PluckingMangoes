73b4c181e4bc5638d22c169b75dd4083287f9f92
NeeharYamani <74959911+NeeharYamani@users.noreply.github.com>
2021-01-17 17:41:43 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,30 @@
+class Launcher{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.04,
+            length:10
+        }
+        
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world,this.sling)
+    }
+    fly(){
+      this.sling.bodyA = null  
+    }
+    attach(body){
+        this.sling.bodyA = body;
+    }
+    display(){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+           
+            stroke("black")
+                strokeWeight(4);
+                line(pointA.x,pointA.y,pointB.x,pointB.y)
+            
+        
+    }
+    }
